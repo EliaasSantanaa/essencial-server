09833565bb146d41790aa46660313aac3924c429
EliaasSantanaa <eliaassantana00@gmail.com>
2025-09-30 20:32:12 -0300

    fix: adjusting firebase-admin

--- src/firebase/firebase-admin.config.ts
+++ src/firebase/firebase-admin.config.ts
@@ -1,9 +1,11 @@
 import * as admin from 'firebase-admin';
-import { getFirestore } from 'firebase-admin/firestore'; // 👈 1. Importe o getFirestore
+import { getFirestore } from 'firebase-admin/firestore';
 import * as serviceAccount from './service-account.json';
 
+const serviceAccountObject = JSON.parse(JSON.stringify(serviceAccount));
+
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccountObject),
 });
 
 const db = getFirestore();
