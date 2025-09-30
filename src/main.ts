08645a2432d37f0329ecf8d58384904f5f4f2bef
EliaasSantanaa <eliaassantana00@gmail.com>
2025-09-30 20:40:31 -0300

    feat: new features

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
 
   const port = configService.get('PORT') ?? 3000;
   await app.listen(port);
