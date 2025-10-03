f5fac6bdf530f86c284ff310c0e6eed8b43e6988
EliaasSantanaa <eliaassantana00@gmail.com>
2025-10-03 19:54:58 -0300

    feat: adjusting return

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -57,7 +57,7 @@ export class AuthController {
 
     try {
       await this.authService.verifyEmailAndActivateUser(oobCode);
-      return res.redirect('https://essencial-dev.vercel.app/sign-in');
+      return res.redirect('https://essencial-dev.vercel.app/signin');
     } catch (error) {
       return res.redirect('https://essencial-dev.vercel.app/falha-ativacao');
     }
