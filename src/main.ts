c0752f80d0b15d752cc2d4ab954c29b8becebeee
Nathan Freitas <nrodriguesdefreitas@gmail.com>
2025-10-19 21:35:23 -0300

    feat: configure documentation with Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Essencial Server API')
+    .setDescription('Documentação da API do Essencial Server')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
