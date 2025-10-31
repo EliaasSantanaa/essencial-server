69a4e95757c05607ef622600a5baef6533752cc7
EliaasSantanaa <eliaassantana00@gmail.com>
2025-10-30 23:12:50 -0300

    feat: atualizando main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
-  // 1. DEFINA O PREFIXO GLOBAL
-  // Isso fará com que todas as rotas (ex: /patient)
-  // agora sejam acessadas como /api/patient
-  app.setGlobalPrefix('api');
-
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Essencial Server API')
     .setDescription('Documentação da API do Essencial Server')
@@ -20,9 +14,6 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-
-  // 2. MUDE A ROTA DO SWAGGER PARA APENAS 'docs'
-  // O resultado final continuará sendo /api/docs (por causa do prefixo global)
   SwaggerModule.setup('docs', app, document);
 
   app.enableCors({
@@ -36,8 +27,6 @@ async function bootstrap() {
   const port = configService.get('PORT') ?? 3002;
   await app.listen(port);
   console.log(`Server is running at http://localhost:${port}`);
-  
-  // Atualize os logs para refletir a mudança
   console.log(`Swagger docs is running at http://localhost:${port}/api/docs`);
 }
 bootstrap();
