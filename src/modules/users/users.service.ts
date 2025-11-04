fa2bd6de59c07a4deed9e63722b41ed478983122
EliaasSantanaa <eliaassantana00@gmail.com>
2025-11-03 21:56:46 -0300

    feat: implementando modulo users

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { firestoreDb } from '../../firebase/firebase-admin.config';
+
+@Injectable()
+export class UsersService {
+    constructor() {}
+
+
+}
