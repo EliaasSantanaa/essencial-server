fce9ff4afe6d6a3294002bf4add1db9fbf8b7b72
EliaasSantanaa <eliaassantana00@gmail.com>
2025-11-10 21:30:45 -0300

    feat: implementando rotas do user

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Delete, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Put } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll() {
+    const users = await this.usersService.findAll();
+    return users;
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findOne(id);
+    return user;
+  }
 
 
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    const user = await this.usersService.update(id, updateUserDto);
+    return user;
+  }
 }
