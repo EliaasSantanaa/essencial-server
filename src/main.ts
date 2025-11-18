edf9c61e3c04e371956b600fdfe465b8fe992b79
EliaasSantanaa <eliaassantana00@gmail.com>
2025-11-18 20:23:24 -0300

    feat: enabling cors

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
   }
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://essencial-dev.vercel.app'],
+    origin: ['http://localhost:5173', 'https://essencial-dev.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
